f35d4c824ababdac05a6b859b35e4ae5988cc419
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-24 22:35:28 +0300

    Backend: Routes: Add register endpoint to v1.0.0 API version group

--- farmduino-server/routes/api.php
+++ farmduino-server/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["prefix" => "v1.0.0"], function () {
+
+    Route::post('/register', [RegisterController::class, "register"]);
+
 });
