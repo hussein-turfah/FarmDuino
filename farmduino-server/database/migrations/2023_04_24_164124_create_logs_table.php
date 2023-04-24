e2a131fcf1cf77aada17d9864d0f958d29068dcd
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-24 21:00:33 +0300

    Backend: Migrations: Create logs table with component, message, greenhouse_id, and greenhouse_user_id columns and their respective foreign keys.

---
+++ farmduino-server/database/migrations/2023_04_24_164124_create_logs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('component', 45);
+            $table->string('message', 200);
+            $table->unsignedBigInteger('greenhouses_id');
+            $table->unsignedBigInteger('greenhouses_users_id');
+            $table->foreign('greenhouses_id')->references('id')->on("greenhouses");
+            $table->foreign('greenhouses_users_id')->references('users_id')->on("greenhouses");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('logs');
+    }
+};
