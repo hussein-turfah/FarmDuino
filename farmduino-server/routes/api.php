b2603c2b8eab31289691cbe00b10768360acb562
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-25 06:09:49 +0300

    Backend: Routes: Add new-password endpoint to v1.0.0 API version group

--- farmduino-server/routes/api.php
+++ farmduino-server/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\NewPasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,4 @@ Route::group(["prefix" => "v1.0.0"], function () {
 
     Route::post('/register', [RegisterController::class, "register"]);
     Route::post('/login', [LoginController::class, "login"]);
-
-});
+    Route::post('/new-password', [NewPasswordController::class, "newPassword"]);});
