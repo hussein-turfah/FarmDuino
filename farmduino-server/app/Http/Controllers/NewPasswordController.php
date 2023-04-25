ce0c1b716446bbd90b3509a061d883f57d5feb2f
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-25 06:33:07 +0300

    Backend: Controllers: Add a new checkColor function to NewPasswordController to check user's favorite color.

--- farmduino-server/app/Http/Controllers/NewPasswordController.php
+++ farmduino-server/app/Http/Controllers/NewPasswordController.php
@@ -27,4 +27,23 @@ class NewPasswordController extends Controller
             "message" => "Password changed successfully"
         ], 200);
     }
+    // this function is for checking the user's favorite color as a security measure
+    function checkColor(Request $request){
+        $functions_controller = new functionscontroller();
+
+        $email = $functions_controller -> entryValidate($request ->email);
+        $color = $functions_controller -> entryValidate($request->favorite_color);
+        
+        $user = User::where('email', $email)->first();
+        if (!$user||$user->favorite_color !== $color) {
+            return response()->json([
+                'error' => 'Wrong Email or Favorite Color'
+            ], 401);
+        }else{
+            $jwt_token = Auth::fromUser($user);
+            return response()->json([
+                'token' => $jwt_token
+            ], 200);
+        }
+    }
 }
