9d2c68c2eec4a2eb6508e45a5a17bcad3f45043e
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-26 23:34:46 +0300

    Backend: Controllers: Functions: Add cURL library to get data from the url.

--- farmduino-server/app/Http/Controllers/PlantController.php
+++ farmduino-server/app/Http/Controllers/PlantController.php
@@ -6,9 +6,11 @@ use Illuminate\Http\Request;
 
 class PlantController extends Controller
 {
-    //function to get all plants
+    #function to get all plants
     public function getPlants(){
         $api_key = getenv('TREFLE_API_KEY');
-        $url = "https://trefle.io/api/v1/plants?token=$api_key";
+        $url = "https://trefle.io/api/v1/plants?token=$api_key";        
+        //use the curl library to get data from api
+        $ch = curl_init();
     }
 }
