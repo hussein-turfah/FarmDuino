71eddadc0ab709f498672071f5b2ac953ebda012
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-26 16:05:39 +0300

    Backend: Controllers: Functions: Add comments for better readability understandability. :brain: :eyes:

--- farmduino-server/app/Http/Controllers/WeatherController.php
+++ farmduino-server/app/Http/Controllers/WeatherController.php
@@ -8,12 +8,16 @@ class WeatherController extends Controller
 {
     #a function to get the weather from an API
     public function getWeather(){
+        //get the API key and the longitude and latitude from the .env file
         $api_key = getenv('WEATHER_API_KEY');
         $longitude = getenv('LONGITUDE');
         $latitude = getenv('LATITUDE');
+        //make the API call
         $url = "https://api.openweathermap.org/data/2.5/forecast?lat=".$latitude."&lon=".$longitude."&appid=".$api_key."&units=metric";
+        // decode the response
         $response = file_get_contents($url);
         $response = json_decode($response);
+        //return the details we need as an array
         $return_details = array();
         $return_details['icon'] = $response->list[0]->weather[0]->icon;
         $return_details['description'] = $response->list[0]->weather[0]->description;
