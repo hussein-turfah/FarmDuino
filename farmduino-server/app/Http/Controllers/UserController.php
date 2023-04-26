7489369e72400e73f5536fc12342a91f76243d87
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-26 08:19:10 +0300

    Backend: Controllers: Functions: Add changeEmail function.

--- farmduino-server/app/Http/Controllers/UserController.php
+++ farmduino-server/app/Http/Controllers/UserController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-   
+    // change the email of the user
+    public function changeEmail(Request $request){
+        
+    }
     
 }
