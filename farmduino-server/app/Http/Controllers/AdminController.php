e55a779a1ea20c2bf2980e1c000b68a467834ba6
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-26 09:26:19 +0300

    Backend: Controllers: Functions: Add listUsers function.

--- farmduino-server/app/Http/Controllers/AdminController.php
+++ farmduino-server/app/Http/Controllers/AdminController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    //a function for listing all users
+    public function listUsers(){
+    }
 }
