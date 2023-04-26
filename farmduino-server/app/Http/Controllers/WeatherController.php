5d91facad5b77bc0291881aa75f1b5e46c2bef05
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-26 15:11:24 +0300

    Backend: Controllers: Functions: Add a method to import WEATHER_API_KEY from .env file for security purposes.

--- farmduino-server/app/Http/Controllers/WeatherController.php
+++ farmduino-server/app/Http/Controllers/WeatherController.php
@@ -8,5 +8,6 @@ class WeatherController extends Controller
 {
     #a function to get the weather from an API
     public function getWeather(){
+        $api_key = getenv('WEATHER_API_KEY');
     }
 }
