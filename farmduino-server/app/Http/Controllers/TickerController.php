96f5eeb5e6e23e4491c32a106d852db76529de6b
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-28 01:59:53 +0300

    Backend: Controllers: Add create element if it doesn't already exist in the table.

--- farmduino-server/app/Http/Controllers/TickerController.php
+++ farmduino-server/app/Http/Controllers/TickerController.php
@@ -9,5 +9,12 @@ class TickerController extends Controller
 {
     public function addTickerElement(Request $request){
         $ticker_elements = Ticker_Element::where('product_name', $request->product_name)->first();
+        if(!$ticker_elements){
+            $ticker_elements = new Ticker_Element;
+            $ticker_elements->product_name = $request->product_name;
+            $ticker_elements->price = $request->price;
+            $ticker_elements->save();
+            return response()->json($ticker_elements, 200);
+        }
     }
 }
