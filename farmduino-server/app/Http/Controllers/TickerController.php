00de5a475edb8d463deb38392a6678ed88eba534
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-28 00:46:11 +0300

    Backend: Controllers: Create addTickerElement function.

--- farmduino-server/app/Http/Controllers/TickerController.php
+++ farmduino-server/app/Http/Controllers/TickerController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class TickerController extends Controller
 {
-    //
+    //add to ticker_elements table
+    public function addTickerElement(Request $request){
+    }
 }
