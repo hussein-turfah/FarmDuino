d090b6d53b7ccc44847a2e81be83c5eba6ebe089
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-28 06:42:10 +0300

    Backend: Controllers: Add logic 🧠 for listSensorData function to list📋 sensor data.

--- farmduino-server/app/Http/Controllers/SensorController.php
+++ farmduino-server/app/Http/Controllers/SensorController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sensor;
 use Illuminate\Http\Request;
 
 class SensorController extends Controller
 {
     # a function for listing sensor data
     public function listSensorData(){
+        $sensorData = Sensor::all();
+        return response()->json($sensorData, 200);
     }
 }
