7ddc799f706d7eece8810b0df795197f3140f5c0
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-28 06:26:54 +0300

    Backend: Controllers: Add function logic 🧠 to list 📋 all logs.

--- farmduino-server/app/Http/Controllers/LogController.php
+++ farmduino-server/app/Http/Controllers/LogController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Log;
 use Illuminate\Http\Request;
 
 class LogController extends Controller
 {
     #a function that lists all logs
     public function listLogs(){
+        $logs = Log::all();
+        return response()->json($logs, 200);
     }
 }
