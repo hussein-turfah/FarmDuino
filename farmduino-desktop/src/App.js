241ac86716bc1a8cc33470808d969bee6f4991e3
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-01 02:29:19 +0300

    🔥Frontend: Add new routes to app.js🤩

--- farmduino-desktop/src/App.js
+++ farmduino-desktop/src/App.js
@@ -1,12 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import './App.css';
+import './reset.css'
 
-import Register from './pages/register';
+
+import Register from './pages/register/register';
+import Login from "./pages/login/login";
+import Dashboard from "./pages/dashboard/dashboard";
 
 function App() {
   return (
       <Routes>
-        <Route path="/" element={<Register/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/" element={<Dashboard/>} />
       </Routes>
   );
 }
