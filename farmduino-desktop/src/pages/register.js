956887666a4cc444ed9399a7555b3a1b8691ab33
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-30 11:29:34 +0300

    ⚛️  Frontend: Add required attributes for all required input fields:fire:

--- farmduino-desktop/src/pages/register.js
+++ farmduino-desktop/src/pages/register.js
@@ -6,23 +6,23 @@ const Register = () => {
       <h1>Register</h1>
       <div>
         <div>
-          <TextField id="outlined-basic" label="First Name" variant="outlined" />
-          <TextField id="outlined-basic" label="Last Name" variant="outlined" />
+          <TextField id="outlined-basic" label="First Name" variant="outlined" required/>
+          <TextField id="outlined-basic" label="Last Name" variant="outlined" required/>
         </div>
         <div>
-          <TextField id="outlined-basic" label="Email" variant="outlined" type= "email" />
+          <TextField id="outlined-basic" label="Email" variant="outlined" type= "email" required/>
         </div>
         <div>
-          <TextField id="outlined-basic" label="Password" variant="outlined" type="password" />
+          <TextField id="outlined-basic" label="Password" variant="outlined" type="password" required/>
         </div>
         <div>
-          <TextField id="outlined-basic" label="Date of Birth" variant="outlined" type="date" />
+          <TextField id="outlined-basic" label="Date of Birth" variant="outlined" type="date" required/>
         </div> 
         <div>
-          <TextField id="outlined-basic" label="Favorite Color" variant="outlined"/>
+          <TextField id="outlined-basic" label="Favorite Color" variant="outlined" required/>
         </div>
         <div>
-          <TextField id="outlined-basic" label="Plant Name" variant="outlined"/>
+          <TextField id="outlined-basic" label="Plant Name" variant="outlined" required/>
         </div>
       </div>
     </div>
