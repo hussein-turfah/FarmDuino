36a616ba2e7bbc9b84f697b510fc2384bfe77375
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-30 14:26:10 +0300

    :fire: Frontend: Pages: Login: Add class names to be used in stylings🎨

--- farmduino-desktop/src/pages/login.js
+++ farmduino-desktop/src/pages/login.js
@@ -2,19 +2,19 @@ import { TextField, Button, Link } from "@mui/material";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <div>
-        <div>
+    <div className="main-container">
+      <h2>Login</h2>
+      <div className="submain-container">
+        <div className="field-container">
           <TextField id="outlined-basic" label="Email" variant="outlined" type="email" required/>
         </div>
-        <div>
+        <div className="field-container">
           <TextField id="outlined-basic" label="Password" variant="outlined" type="password" required/>
         </div>
-        <div>
+        <div className="button-container">
           <Button variant="outlined">Login</Button>
         </div>
-        <div>
+        <div className="links-container">
           <Link href="/forgot-password">Forgot Password?</Link>
           <Link href="/register">Don't have an account? Register</Link>
         </div>
