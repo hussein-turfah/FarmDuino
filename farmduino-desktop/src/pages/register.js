c3654952739b00eda844b839786b74de0e9a3227
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-30 11:15:13 +0300

    ⚛️  Frontend: Add Material UI fields in Registeration page:fire:

--- farmduino-desktop/src/pages/register.js
+++ farmduino-desktop/src/pages/register.js
@@ -1,7 +1,15 @@
+import TextField from '@mui/material/TextField';
+
 const Register = () => {
   return (
     <div>
       <h1>Register</h1>
+      <div>
+        <div>
+          <TextField id="outlined-basic" label="First Name" variant="outlined" />
+          <TextField id="outlined-basic" label="Last Name" variant="outlined" />
+        </div>
+      </div>
     </div>
   );
 };
