dc787c3f1249cdadad021606ea78253fd21e4e8e
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-30 11:23:09 +0300

    ⚛️  Frontend: Add MUI date of birth field and types of several fields:fire:

--- farmduino-desktop/src/pages/register.js
+++ farmduino-desktop/src/pages/register.js
@@ -10,12 +10,14 @@ const Register = () => {
           <TextField id="outlined-basic" label="Last Name" variant="outlined" />
         </div>
         <div>
-          <TextField id="outlined-basic" label="Email" variant="outlined" />
+          <TextField id="outlined-basic" label="Email" variant="outlined" type= "email" />
         </div>
         <div>
-          <TextField id="outlined-basic" label="Password" variant="outlined" />
+          <TextField id="outlined-basic" label="Password" variant="outlined" type="password" />
         </div>
-        
+        <div>
+          <TextField id="outlined-basic" label="Date of Birth" variant="outlined" type="date"/>
+        </div>  
       </div>
     </div>
   );
