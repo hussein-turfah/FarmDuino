fac3ce50e53c773c9cc1ea5bd9ce7a3e1c30727a
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-30 12:50:07 +0300

    ⚛️ Frontend: Pages: Components: Add login button and clean file formatting in login page.🔐

--- farmduino-desktop/src/pages/login.js
+++ farmduino-desktop/src/pages/login.js
@@ -1,16 +1,21 @@
 import { TextField } from "@mui/material";
 
 const Login = () => {
-    return (
+  return (
+    <div>
+      <h1>Login</h1>
+      <div>
         <div>
-            <h1>Login</h1>
-            <div>
-              <TextField id="outlined-basic" label="Email" variant="outlined" type= "email" required/>
-            </div>
-            <div>
-              <TextField id="outlined-basic" label="Password" variant="outlined" type="password" required/>
-            </div>
+          <TextField id="outlined-basic" label="Email" variant="outlined" type="email" required/>
         </div>
-    )
-}
+        <div>
+          <TextField id="outlined-basic" label="Password" variant="outlined" type="password" required/>
+        </div>
+        <div>
+          <Button variant="outlined">Register</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Login;
