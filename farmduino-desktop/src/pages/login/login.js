caf80015d07925b939430420d3c459505d27199a
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-30 23:55:03 +0300

    🔥Frontend: 🎨 Style: Create login.module.css for scopping css styles.

--- farmduino-desktop/src/pages/login/login.js
+++ farmduino-desktop/src/pages/login/login.js
@@ -1,19 +1,20 @@
 import { TextField, Button, Link } from "@mui/material";
+import styles from "./login.module.css";
 
 const LoginImage = process.env.PUBLIC_URL + '/assets/images/login.jpg';
 
 const Login = () => {
   return (
-    <div className="body">
-      <div className="image-container">
-        <img src={LoginImage} alt="Login" className="image" />
+    <div className={styles.body}>
+      <div className={styles.image_container}>
+        <img src={LoginImage} alt="Login" className={styles.image} />
       </div>
-      <div className="main-container">
-        <h1 className="h1">Login</h1>
-        <div className="submain-container">
-          <div className="textfield-container">
-            <TextField className="field" id="outlined-basic" label="Email" variant="outlined" type="email" required/>
-            <TextField className="field" id="outlined-basic" label="Password" variant="outlined" type="password" required/>
+      <div className={styles.main_container}>
+        <h1 className={styles.h1}>Login</h1>
+        <div className={styles.submain_container}>
+          <div className={styles.textfield_container}>
+            <TextField className={styles.field} id="outlined-basic" label="Email" variant="outlined" type="email" required/>
+            <TextField className={styles.field} id="outlined-basic" label="Password" variant="outlined" type="password" required/>
           </div>
             <Button variant="outlined">Login</Button>
             <Link href="/">Forgot Password?</Link>
