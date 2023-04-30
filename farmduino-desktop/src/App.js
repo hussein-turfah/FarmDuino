e7af575468f384e366b04be8300a75c2bf143a4d
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-30 10:46:46 +0300

    ⚛️  Frontend: Add routing to App.js and index.js files:fire:

--- farmduino-desktop/src/App.js
+++ farmduino-desktop/src/App.js
@@ -1,8 +1,13 @@
-import logo from './logo.svg';
+import { Routes, Route } from "react-router-dom";
 import './App.css';
+
+import Register from './pages/register';
 
 function App() {
   return (
+      <Routes>
+        <Route path="/" element={<Register/>} />
+      </Routes>
   );
 }
 export default App;
