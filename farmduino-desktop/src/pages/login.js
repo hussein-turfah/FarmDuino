568063a6adca9572e633f420000dc57e5fb6057a
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-30 12:41:56 +0300

    ⚛️ Frontend: Pages: Components: Add email text field to the login page.:star:

--- farmduino-desktop/src/pages/login.js
+++ farmduino-desktop/src/pages/login.js
@@ -1,7 +1,12 @@
+import { TextField } from "@mui/material";
+
 const Login = () => {
     return (
         <div>
             <h1>Login</h1>
+            <div>
+              <TextField id="outlined-basic" label="Email" variant="outlined" type= "email" required/>
+            </div>
         </div>
     )
 }
