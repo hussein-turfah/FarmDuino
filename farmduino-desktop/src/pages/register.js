a4fb166f3dd935084c3fe10c0028846f83c0e74d
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-30 14:21:16 +0300

    ⚛️ Frontend: Pages: Register:🎨 Style: Reformat imports and remove unused imports.

--- farmduino-desktop/src/pages/register.js
+++ farmduino-desktop/src/pages/register.js
@@ -1,14 +1,10 @@
 import * as React from 'react';
-import TextField from '@mui/material/TextField';
-import Button  from '@mui/material/Button';
+import { TextField, Button } from '@mui/material/';
 import dayjs from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers';
-
+import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers/';
 
 const Register = () => {
-  // const [locale, setLocale] = React.useState('zh-cn');
   const locale = 'zh-cn';
   
   return (
@@ -36,9 +32,9 @@ const Register = () => {
         <div>
           <TextField id="outlined-basic" label="Plant Name" variant="outlined" required/>
         </div>
-      </div>
-      <div>
-        <Button variant="outlined">Register</Button>
+        <div>
+          <Button variant="outlined">Register</Button>
+        </div>
       </div>
     </div>
   );
