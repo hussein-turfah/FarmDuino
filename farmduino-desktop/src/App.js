8823f0af450fd3d09af05bf116bd36c339232bd1
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-30 10:03:54 +0300

    ⚛️  Frontend: Clean React code to start building the frontend.

--- farmduino-desktop/src/App.js
+++ farmduino-desktop/src/App.js
@@ -3,23 +3,6 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>World</code>! My name is <code>Hussein</code> and I'm going to master <code>React</code>!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
-    </div>
   );
 }
-
 export default App;
