899961c74129267cf99f39ad705f0e0703909e4f
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-01 02:37:22 +0300

    🔥Frontend: Create new Dashboard page and add a css file for its styles🤩

---
+++ farmduino-desktop/src/pages/dashboard/dashboard.js
@@ -0,0 +1,10 @@
+import styles from './dashboard.module.css';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+        </div>
+    )
+}
+export default Dashboard;
