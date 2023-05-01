f629f0dff96fa94c3e3aaf29236374b5a8790594
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-01 17:47:12 +0300

    :fire:Frontend: Pages: Dashboard: Add several new elements to the Dashboard page

--- farmduino-desktop/src/pages/dashboard/dashboard.js
+++ farmduino-desktop/src/pages/dashboard/dashboard.js
@@ -1,6 +1,7 @@
 import Sidebar from '../../components/user-sidebar/sidebar';
 import styles from './dashboard.module.css';
 import Navbar from '../../components/user-navbar/navbar';
+import Ticker from '../../components/ticker/ticker';
 
 const Dashboard = () => {
     return (
@@ -8,6 +9,24 @@ const Dashboard = () => {
             <Sidebar />
             <div className={styles.main_container}>
                 <Navbar />
+                <Ticker />
+                <div>
+                    <div className={styles.page_header}>
+                        <h1>Dashboard</h1>
+                        <h3>Greenhouse 1</h3>
+                    </div>
+                    <div className={styles.container_header}>
+                        <h2>
+                            Greenhouse Details
+                        </h2>
+                        <div className={styles.boxes_container}>
+                            <div className={styles.box}>
+                                <h3>Temperature</h3>
+                                <h4>25°C</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
