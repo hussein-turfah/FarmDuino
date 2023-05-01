17fb98d2f48417933131733509e9a338b91bec10
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-01 22:52:57 +0300

    :fire:Frontend: Pages: Dashboard: Add several elements to the Dashboard page

--- farmduino-desktop/src/pages/dashboard/dashboard.js
+++ farmduino-desktop/src/pages/dashboard/dashboard.js
@@ -5,7 +5,7 @@ import Ticker from '../../components/ticker/ticker';
 import { Page_Content_Container, Page_Title, Mybox, Container_title } from '../../components/general-components/general';
 
 const temperature = process.env.PUBLIC_URL + 'assets/icons/temperature.png';
-
+const plant_image = process.env.PUBLIC_URL + 'assets/images/banana.jpg';
 
 const Dashboard = () => {
     return (
@@ -26,9 +26,26 @@ const Dashboard = () => {
                 </div>
             </div>
             <div className={styles.container2}>
-                <Page_Content_Container className={styles.actuator}
+                <Page_Content_Container className={styles.box1}
+                children = {<>heyy</>}
                 />
                 <Page_Content_Container className={styles.plant_details}
+                children = {
+                    <div>
+                        <img src={plant_image} />
+                        <h2>
+                            Plant Species: 
+                            <h3>
+                            <ul>
+                                <li>Temperature: </li>
+                                <li>Humidity</li>
+                                <li>Soil Moisture</li>
+                                <li>Light Intensity</li>
+                            </ul>
+                            </h3>
+                        </h2>
+                    </div>
+                }
                 />
             </div>
           </div>
