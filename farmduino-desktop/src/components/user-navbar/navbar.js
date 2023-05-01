dd64b5064781abba1d22a94b03a45f8bc34d9134
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-01 12:12:41 +0300

    🔥Frontend: Components: Navbar: Add some Navbar attributes.🤩

--- farmduino-desktop/src/components/user-navbar/navbar.js
+++ farmduino-desktop/src/components/user-navbar/navbar.js
@@ -3,6 +3,14 @@ const Navbar = () => {
 
     return (
         <div className={styles.body}>
+          <div className={styles.main_container}>
+            <div className={styles.logo_container}>
+              <img  alt ="logo" className={styles.logo} />
+            </div>
+            <div className={styles.logo_container}>
+              <img alt="logo" className={styles.logo} />
+            </div>
+          </div>
         </div>
     )
 }
