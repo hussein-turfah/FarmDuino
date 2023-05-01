fc452ea16e330ad63a1a6f90df9f000a799a5457
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-01 11:35:10 +0300

    🔥Frontend: Pages: Import the new Navbar component🤩

--- farmduino-desktop/src/pages/dashboard/dashboard.js
+++ farmduino-desktop/src/pages/dashboard/dashboard.js
@@ -1,10 +1,12 @@
 import Sidebar from '../../components/user-sidebar/sidebar';
 import styles from './dashboard.module.css';
+import Navbar from '../../components/user-navbar/navbar';
 
 const Dashboard = () => {
     return (
         <div>
-            <Sidebar className={styles.body}/>
+            <Sidebar />
+            <Navbar />
         </div>
     )
 }
