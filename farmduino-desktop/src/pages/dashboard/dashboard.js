3f61982f5c059582b0ca7988dd7d694566287bc6
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-02 02:10:59 +0300

    :fire:Frontend: Pages: Dashboard: Add several new elements to the Dashboard page

--- farmduino-desktop/src/pages/dashboard/dashboard.js
+++ farmduino-desktop/src/pages/dashboard/dashboard.js
@@ -3,9 +3,11 @@ import styles from './dashboard.module.css';
 import Navbar from '../../components/user-navbar/navbar';
 import Ticker from '../../components/ticker/ticker';
 import { Page_Content_Container, Page_Title, Mybox, Container_title } from '../../components/general-components/general';
+import React from 'react';
+import Plant from '../../components/dashboard-components/plant';
+import Actuators from '../../components/dashboard-components/actuators';
 
 const temperature = process.env.PUBLIC_URL + 'assets/icons/temperature.png';
-const plant_image = process.env.PUBLIC_URL + 'assets/images/banana.jpg';
 
 const Dashboard = () => {
     return (
@@ -26,27 +28,14 @@ const Dashboard = () => {
                 </div>
             </div>
             <div className={styles.container2}>
-                <Page_Content_Container className={styles.box1}
-                children = {<>heyy</>}
-                />
-                <Page_Content_Container className={styles.plant_details}
-                children = {
-                    <div>
-                        <img src={plant_image} />
-                        <h2>
-                            Plant Species: 
-                            <h3>
-                            <ul>
-                                <li>Temperature: </li>
-                                <li>Humidity</li>
-                                <li>Soil Moisture</li>
-                                <li>Light Intensity</li>
-                            </ul>
-                            </h3>
-                        </h2>
-                    </div>
-                }
-                />
+                <Actuators />
+                <Plant/>    
+            </div>
+            <div className={styles.container2}>
+                <Container_title title='Weather Forecast'/>
+                <div className={styles.cards_container}>
+                    <div></div>
+                </div>
             </div>
           </div>
         </div>
