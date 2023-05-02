85d3e839f3e96f86affc7855bd85fa480e4d5d4f
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-02 12:41:41 +0300

    :fire:Frontend: Add new routes

--- farmduino-desktop/src/App.js
+++ farmduino-desktop/src/App.js
@@ -6,13 +6,21 @@ import './reset.css'
 import Register from './pages/register/register';
 import Login from "./pages/login/login";
 import Dashboard from "./pages/dashboard/dashboard";
+import Temperature from "./pages/temperature/temperature";
+import Humidity from "./pages/humidity/humidity";
+import Soil_moisture from "./pages/soil-moisture/soil-moisture";
+import Light_intensity from "./pages/light-intensity/light-intensity";
 
 function App() {
   return (
       <Routes>
         <Route path="/register" element={<Register/>} />
         <Route path="/login" element={<Login/>} />
-        <Route path="/" element={<Dashboard/>} />
+        <Route path="*" element={<Dashboard/>} />
+        <Route path="/temperature" element={<Temperature/>} />
+        <Route path="/humidity" element={<Humidity/>} />
+        <Route path="/soil-moisture" element={<Soil_moisture/>} />
+        <Route path="/light-intensity" element={<Light_intensity/>} />
       </Routes>
   );
 }
