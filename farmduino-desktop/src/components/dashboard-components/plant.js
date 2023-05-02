b36a3c0983119644f45db83a928b4ec1c511677e
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-02 14:54:14 +0300

    :wrench:Frontend: :star:Components: Fix the arrangement of some elements in the plant component

--- farmduino-desktop/src/components/dashboard-components/plant.js
+++ farmduino-desktop/src/components/dashboard-components/plant.js
@@ -6,27 +6,19 @@ const plant_image = process.env.PUBLIC_URL + 'assets/images/banana.jpg';
 
 export const Plant = (props) => {
   return (
-    <div className={styles.plant_details}>
-      <Page_Content_Container
-        children={
-          <div className={styles.plant_details_container}>
-            <Container_title title="Plant Details" />
-            <div className={styles.plant_image_container}>
-              <img src={plant_image} className={styles.plant_image}/>
-            </div>
-            <h3>
-              Plant Species:
-              <ul>
-                <li>Temperature: </li>
-                <li>Humidity:</li>
-                <li>Soil Moisture:</li>
-                <li>Light Intensity:</li>
-              </ul>
-            </h3>
-          </div>
-        }
-      />
-    </div>
+      <div className={styles.plant_details_container}>
+        <div className={styles.plant_image_container}>
+          <img src={plant_image} className={styles.plant_image} />
+        </div>
+        <div className={styles.details}>
+          <Container_title title="Plant Details" />
+          <h3>&nbsp;Plant Species:</h3>
+          <h3>&nbsp;&nbsp;Temperature: </h3>
+          <h3>&nbsp;&nbsp;Humidity:</h3>
+          <h3>&nbsp;&nbsp;Soil Moisture:</h3>
+          <h3>&nbsp;&nbsp;Light Intensity:</h3>
+        </div> 
+      </div>
   );
 };
 
