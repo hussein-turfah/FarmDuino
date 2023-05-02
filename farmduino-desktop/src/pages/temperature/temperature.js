85c1c3feebb7e1a3c50f6420edfc8ac1109445de
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-02 12:29:36 +0300

    :rocket: Frontend: Pages: Temperature: Add new elements to the temperature page :fire:

--- farmduino-desktop/src/pages/temperature/temperature.js
+++ farmduino-desktop/src/pages/temperature/temperature.js
@@ -3,28 +3,27 @@ import Sidebar from '../../components/user-sidebar/sidebar';
 import Navbar from '../../components/user-navbar/navbar';
 import Ticker from '../../components/ticker/ticker';
 import { Page_Title } from '../../components/general-components/general';
-import Plant from '../../components/dashboard-components/plant';
+import {Plant_row} from '../../components/dashboard-components/plant';
+import styles from './temperature.module.css';
 
-const MyLineGraph = () => {
-
-    return (
-      <div className='body'>
-        <Sidebar />
-        <div className='main_container'>
-          <Navbar />
-          <Ticker />
-          <div className='submain_container'>
-            <Page_Title title="Temperature" subtitle="Greenhouse 1" />
-            <div className='graph container'>
-            </div>
-            <Plant />
+const Temperature = () => {
+  return (
+    <div className="body">
+      <Sidebar />
+      <div className="main_container">
+        <Navbar />
+        <Ticker />
+        <div className="submain_container">
+          <Page_Title title="Temperature" subtitle="Greenhouse 1" />
+          <div className="graph container"></div>
+          <div className={styles.plant}>
+            <Plant_row />
           </div>
         </div>
-        
       </div>
-    );
-  
-}
+    </div>
+  );
+};
 
-export default MyLineGraph;
+export default Temperature;
 
