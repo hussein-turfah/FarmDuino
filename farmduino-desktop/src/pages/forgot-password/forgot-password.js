2383e470fba782aede60dde34d69aaa0129840f4
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-04 02:11:53 +0300

    :rocket:Frontend: :fire:Pages: Forgot Passwrod: Add new elements to the forgot password page.

--- farmduino-desktop/src/pages/forgot-password/forgot-password.js
+++ farmduino-desktop/src/pages/forgot-password/forgot-password.js
@@ -6,7 +6,23 @@ const forgot_password_image = process.env.PUBLIC_URL + '/assets/images/forgot-pa
 const ForgotPassword = () => {
   return (
     <div className={styles.body}>
-     
+       <div className={styles.image_container}>
+        <img src={forgot_password_image} alt="ForgotPassword" className={styles.image} />
+      </div>
+      <div className={styles.main_container}>
+        <h1 className={styles.h1}>Forgot Password</h1>
+        <div className={styles.submain_container}>
+          <div className={styles.textfield_container}>
+            <TextField className={styles.field} id="outlined-basic" label="Email" variant="outlined" type="email" required/>
+            <TextField className={styles.field} id="outlined-basic" label="Favorite Color" variant="outlined" type="text" required/>
+          </div>
+            <Button variant="outlined" sx={{background:'#9747FF',height:'100%',width:'40%', padding: '4%', color:'white',":hover":{color:'black',borderColor:'#9747FF'}}}>Reset Password</Button>
+        </div>
+        <div>
+            <p className={styles.links}>Remembered your password? <a href="/login">Login</a></p>
+            <p className={styles.links}>Don't have an account? <a href="/register">Register</a></p>
+        </div>
+      </div>
     </div>
   );
 };
