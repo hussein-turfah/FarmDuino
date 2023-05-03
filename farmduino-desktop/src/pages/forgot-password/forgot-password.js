a14758b9d4d0ca1e89196b835da69547d1d65c59
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-04 01:58:04 +0300

    :wrench:Frontend: Pages: Create a new forgot-password page to handle user requests for resetting their password

---
+++ farmduino-desktop/src/pages/forgot-password/forgot-password.js
@@ -0,0 +1,13 @@
+import { TextField, Button } from "@mui/material";
+import styles from "./forgot-password.module.css";
+
+const forgot_password_image = process.env.PUBLIC_URL + '/assets/images/forgot-password.jpg';
+
+const ForgotPassword = () => {
+  return (
+    <div className={styles.body}>
+     
+    </div>
+  );
+};
+export default ForgotPassword;
