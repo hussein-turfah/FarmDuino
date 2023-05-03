f9fa2b811df78c65b2162fba72aed00bd719a5c9
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-04 02:15:02 +0300

    :broom:Frontend: Login: Use better typography and text elements arrangement.

--- farmduino-desktop/src/pages/login/login.js
+++ farmduino-desktop/src/pages/login/login.js
@@ -16,9 +16,11 @@ const Login = () => {
             <TextField className={styles.field} id="outlined-basic" label="Email" variant="outlined" type="email" required/>
             <TextField className={styles.field} id="outlined-basic" label="Password" variant="outlined" type="password" required/>
           </div>
-            <Button variant="outlined">Login</Button>
-            <Link href="/">Forgot Password?</Link>
-            <Link href="/">Don't have an account? Register</Link>
+            <Button variant="outlined" sx={{background:'#9747FF',height:'100%',width:'30%', padding: '3%', color:'white',":hover":{color:'black',borderColor:'#9747FF'}}}>Login</Button>
+        </div>
+        <div>
+            <p className={styles.links}>Don't have an account? <a href="/register">Register</a></p>
+            <p className={styles.links}><a href="/forgot-password">Reset password</a></p>
         </div>
       </div>
     </div>
