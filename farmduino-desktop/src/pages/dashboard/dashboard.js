5be0a0a44b27d23f9e036b04b8883e652b5455b2
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-03 22:20:17 +0300

    :link: Frontend: Linking: Link the weather container to the weather controller in the backend to get data and style the output in the form of cards

--- farmduino-desktop/src/pages/dashboard/dashboard.js
+++ farmduino-desktop/src/pages/dashboard/dashboard.js
@@ -2,10 +2,11 @@ import Sidebar from '../../components/user-sidebar/sidebar';
 import styles from './dashboard.module.css';
 import Navbar from '../../components/user-navbar/navbar';
 import Ticker from '../../components/ticker/ticker';
-import { Page_Title, Mybox, Container_title } from '../../components/general-components/general';
-import React from 'react';
+import { Page_Title, Mybox, Container_title, My_weather_box } from '../../components/general-components/general';
+import React, { useState, useEffect  } from 'react';
 import { Plant } from '../../components/dashboard-components/plant';
 import Actuators from '../../components/dashboard-components/actuators';
+import UseHttp from '../../hooks/http-request';
 
 const temperature = process.env.PUBLIC_URL + 'assets/icons/temperature.png';
 const humidity = process.env.PUBLIC_URL + 'assets/icons/humidity.png';
@@ -13,6 +14,20 @@ const soil_moisture = process.env.PUBLIC_URL + 'assets/icons/soil_moisture.png';
 const light_intensity = process.env.PUBLIC_URL + 'assets/icons/light_intensity.png';
 
 const Dashboard = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const weather = await UseHttp("weather", "GET");
+        setData(weather);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className='body'>
       <Sidebar />
@@ -24,20 +39,30 @@ const Dashboard = () => {
           <div className={styles.container1}>
             <Container_title title='Greenhouse Details' />
             <div className={styles.cards_container}>
-              <Mybox title="Temperature" image_source={temperature} value="30 C" />
-              <Mybox title="Humidity" image_source={humidity} value="30 C" />
-              <Mybox title="Soil Moisture" image_source={soil_moisture} value="30 C" />
-              <Mybox title="Light Intensity" image_source={light_intensity} value="30 C" />
+              <Mybox styles={styles.mybox} title="Temperature" image_source={temperature} value={'test'} />
+              <Mybox styles={styles.mybox} title="Humidity" image_source={humidity} value="30 C" />
+              <Mybox styles={styles.mybox} title="Soil Moisture" image_source={soil_moisture} value="30 C" />
+              <Mybox styles={styles.mybox} title="Light Intensity" image_source={light_intensity} value="30 C" />
             </div>
           </div>
           <div className={styles.container2}>
-            <div className={styles.actuators}><Actuators /></div>
+            <div className={styles.actuators}><Actuators/></div>
             <div className={styles.plant}><Plant /></div>
           </div>
-          <div className={styles.container2}>
+          <div className={styles.container3}>
             <Container_title title='Weather Forecast' />
             <div className={styles.cards_container}>
-              <div>Temperature external api goes here</div>
+                {data.map((day, index) => (
+                  <My_weather_box styles={styles.my_weather_box}
+                    key={index}
+                    date={day.date}
+                    image_source={`https://openweathermap.org/img/w/${day.icon}.png`}
+                    temperature={`Temperature: ${day.temperature}°C`}
+                    description={`Description: ${day.description}`}
+                    humidity={`Humidity: ${day.humidity}%`}
+                    wind_speed={`Wind speed: ${day.wind_speed} Km/h`}
+                  />
+                ))}
             </div>
           </div>
         </div>
