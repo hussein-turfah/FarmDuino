2f5764de43699cedb6ee9fc7c071fa63272ea9af
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-05 01:07:10 +0300

    :link:Frontend :fire:Link frontend login page to backend

--- farmduino-desktop/src/pages/login/login.js
+++ farmduino-desktop/src/pages/login/login.js
@@ -1,9 +1,32 @@
 import { TextField, Button, Link } from "@mui/material";
 import styles from "./login.module.css";
+import { useNavigate } from "react-router";
+import { useRef } from "react";
+import UseHttp from "../../hooks/http-request";
 
 const LoginImage = process.env.PUBLIC_URL + '/assets/images/login.jpg';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const email_ref=useRef("") 
+  const password_ref=useRef("");
+  
+  const saveToken = async () => {
+    const email = email_ref.current.value;
+    const password = password_ref.current.value;
+    console.log(email, password)
+    
+    const form_data = new FormData();
+
+    form_data.append("email", email);
+    form_data.append("password", password);
+
+    const data = await UseHttp("login", "POST",form_data);
+    if(data.user) {
+      localStorage.setItem('token', data.token);
+      navigate("/dashboard")
+    }
+  }
   return (
     <div className={styles.body}>
       <div className={styles.image_container}>
@@ -13,14 +36,20 @@ const Login = () => {
         <h1 className={styles.h1}>Login</h1>
         <div className={styles.submain_container}>
           <div className={styles.textfield_container}>
-            <TextField className={styles.field} id="outlined-basic" label="Email" variant="outlined" type="email" required/>
-            <TextField className={styles.field} id="outlined-basic" label="Password" variant="outlined" type="password" required/>
+            <TextField className={styles.field} id="outlined-basic" label="Email" variant="outlined" type="email" required inputRef={email_ref} />
+            <TextField className={styles.field} id="outlined-basic" label="Password" variant="outlined" type="password" required inputRef={password_ref} />
           </div>
-            <Button variant="outlined" sx={{background:'#9747FF',height:'100%',width:'30%', padding: '3%', color:'white',":hover":{color:'black',borderColor:'#9747FF'}}}>Login</Button>
+          <Button variant="outlined" sx={{
+            background: '#9747FF', height: '100%', width: '30%', padding: '3%', color: 'white',
+            ":hover": { color: 'black', borderColor: '#9747FF' }
+          }}
+            onClick={saveToken}>
+            Login
+          </Button>
         </div>
         <div>
-            <p className={styles.links}>Don't have an account? <a href="/register">Register</a></p>
-            <p className={styles.links}><a href="/forgot-password">Reset password</a></p>
+          <p className={styles.links}>Don't have an account? <a href="/register">Register</a></p>
+          <p className={styles.links}><a href="/forgot-password">Reset password</a></p>
         </div>
       </div>
     </div>
