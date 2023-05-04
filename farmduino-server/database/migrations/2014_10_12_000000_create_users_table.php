a539f7d134c8c7e8ffff588df2d9c7b305da2e9f
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-05 01:05:03 +0300

    :fire:Backend: Remove unused information from migration details

--- farmduino-server/database/migrations/2014_10_12_000000_create_users_table.php
+++ farmduino-server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('email',45)->unique();
             $table->string('password');
             $table->string('salt',45);
-            $table->string('gender',10);
             $table->date('dob');
             $table->string('favorite_color',45);
             $table->boolean('is_deleted')->default(0);
