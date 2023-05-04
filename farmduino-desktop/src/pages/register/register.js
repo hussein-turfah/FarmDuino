7c6f96bf353c35da898a338652ce183e21c80a15
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-05 01:07:45 +0300

    :link:Frontend :fire:Link frontend Register page to backend

--- farmduino-desktop/src/pages/register/register.js
+++ farmduino-desktop/src/pages/register/register.js
@@ -1,14 +1,50 @@
 import * as React from 'react';
+import { useRef } from 'react';
 import { TextField, Button } from '@mui/material/';
 import dayjs from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers/';
 import styles from './register.module.css';
+import { useNavigate } from 'react-router';
+import UseHttp from '../../hooks/http-request';
 
 const register_image = process.env.PUBLIC_URL + '/assets/images/register.jpg';
 
 const Register = () => {
   const locale = 'zh-cn';
+  const navigate = useNavigate();
+
+  const first_name_ref = useRef("")
+  const last_name_ref = useRef("")
+  const email_ref = useRef("")
+  const password_ref = useRef("")
+  const dob_ref = useRef("")
+  const favorite_color_ref = useRef("")
+  const plant_name_ref = useRef("")
+
+  const sendData = async ()=>{
+    const first_name = first_name_ref.current.value;
+    const last_name = last_name_ref.current.value;
+    const email = email_ref.current.value;
+    const password = password_ref.current.value;
+    const dob = dob_ref.current.value;
+    const favorite_color = favorite_color_ref.current.value;
+    const plant_name = plant_name_ref.current.value;
+
+    const form_data = new FormData();
+    form_data.append("first_name",first_name)
+    form_data.append("last_name",last_name)
+    form_data.append("email",email)
+    form_data.append("password",password)
+    form_data.append("dob",dob)
+    form_data.append("favorite_color",favorite_color)
+    form_data.append("plant_name",plant_name)
+
+    const data = await UseHttp("register","POST",form_data)
+
+    if (data.success) navigate("/login")
+  }
+  
 
   return (
     <div className={styles.body}>
@@ -18,20 +54,19 @@ const Register = () => {
       <div className={styles.main_container}>
         <h1 className={styles.h1}>Register</h1>
         <div className={styles.submain_container}>
-          {/* <div className={styles.input_}></div> */}
           <div className={styles.name_fields}>
-            <TextField className={styles.field} id="outlined-basic" label="First Name" variant="outlined" required/>
-            <TextField className={styles.field} id="outlined-basic" label="Last Name" variant="outlined" required/>
+            <TextField className={styles.field} id="outlined-basic" label="First Name" variant="outlined" required inputRef={first_name_ref}/>
+            <TextField className={styles.field} id="outlined-basic" label="Last Name" variant="outlined" required inputRef={last_name_ref}/>
           </div>
-            <TextField className={styles.textfield_container} id="outlined-basic" label="Email" variant="outlined" type= "email" required/>
-            <TextField className={styles.textfield_container} id="outlined-basic" label="Password" variant="outlined" type="password" required/>
+            <TextField className={styles.textfield_container} id="outlined-basic" label="Email" variant="outlined" type= "email" required inputRef={email_ref}/>
+            <TextField className={styles.textfield_container} id="outlined-basic" label="Password" variant="outlined" type="password" required inputRef={password_ref}/>
             <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={locale}>
-              <DatePicker className={styles.textfield_container} label="Date" defaultValue={dayjs('2023-12-10')} />
+              <DatePicker className={styles.textfield_container} label="Date of Birth" format="YYYY/MM/DD" defaultValue={dayjs('2023-12-10')} required inputRef={dob_ref}/>
             </LocalizationProvider>
-            <TextField className={styles.textfield_container} id="outlined-basic" label="Favorite Color" variant="outlined" required/>
-            <TextField className={styles.textfield_container} id="outlined-basic" label="Plant Name" variant="outlined" required/>
+            <TextField className={styles.textfield_container} id="outlined-basic" label="Favorite Color" variant="outlined" required inputRef={favorite_color_ref}/>
+            <TextField className={styles.textfield_container} id="outlined-basic" label="Plant Name" variant="outlined" required inputRef={plant_name_ref}/>
           <div className={styles.button}>
-            <Button variant='outlined' sx={{background:'#9747FF',height:'100%',width:'50%', padding: '4%', color:'white',":hover":{color:'black',borderColor:'#9747FF'}}}>Register</Button>
+            <Button variant='outlined' sx={{background:'#9747FF',height:'100%',width:'50%', padding: '4%', color:'white',":hover":{color:'black',borderColor:'#9747FF'}}} onClick={sendData}>Register</Button>
           </div>
             <p className={styles.p}>Already have an account? <a href="/login">Login</a></p>
         </div>
