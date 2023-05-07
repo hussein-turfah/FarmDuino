16bc7ed67f75b29188442b4150554a06bfbc7c4c
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-08 01:59:24 +0300

    :star_struck:Arduino: Create a function for sending http post request cointaining sensors values to the required backend route

--- farmduino-arduino/farmduino-arduino.ino
+++ farmduino-arduino/farmduino-arduino.ino
@@ -1,9 +1,9 @@
 #include <DHT.h>
-#include <Wire.h>
+#include <Ethernet.h>
+#include <ArduinoJson.h>
 
 #define DHT_PIN 2
 #define DHT_TYPE DHT22
-#define GY30_ADDR 0x23
 
 
 DHT dht(DHT_PIN, DHT_TYPE);
@@ -12,39 +12,60 @@ int soil_moisture_pin = A0;
 int soil_moisture_value = 0;
 int moisture_percentage = 0;
 int light_intensity = -5; //for demonstrating notifications on frontend
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED}; 
 
+
+EthernetClient client;
+StaticJsonDocument<200> doc;
 
 
 void readDHTData() {
   float temperature = dht.readTemperature();
   float humidity = dht.readHumidity();
-
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.print(" °C, Humidity: ");
-  Serial.print(humidity);
-  Serial.println(" %");
+  doc["temperature"] = temperature;
+  doc["humidity"] = humidity;
 }
 
 
 void readSoilMoisture() {
   soil_moisture_value = analogRead(soil_moisture_pin);
   moisture_percentage = map(soil_moisture_value, 0, 1023, 0, 100);
-  Serial.print("Soil moisture: ");
-  Serial.print(moisture_percentage);
-  Serial.println("%");
+  doc["soil_moisture"] = moisture_percentage;
+}
+
+
+void sendData(String data) {
+  if (client.connect("192.168.0.111", 8000)) { //server ip & port
+    client.println("POST /api/v1.0.0/data HTTP/1.1");
+    client.println("Host: 192.168.0.110"); //arduino ip
+    client.println("Connection: close");
+    client.println("Content-Type: application/json");
+    client.print("Content-Length: ");
+    client.println(data.length());
+    client.println();
+    client.println(data);
+  } else {
+    Serial.println("Connection failed");
+  }
+
+  delay(1000);
+  client.stop();
 }
 
 
 void setup() {
   Serial.begin(9600);
   dht.begin();
+  doc["light_intensity"] = light_intensity;
 }
+
 
 void loop() {
   readDHTData();
   readSoilMoisture();
-  Serial.print(light_intensity);
-  Serial.println(" lux");
+
+  String json_data;
+  serializeJson(doc, json_data);
+  sendData(json_data);
   delay(3000);
 }
