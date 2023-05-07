07ec3c1828bf573b01eca0dd208ea67e79665923
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-08 00:00:04 +0300

    :fire:Arduino: Create a new function for reading temperature and humidity from dht22 sensor and printing them into serial monitor.

--- farmduino-arduino/farmduino-arduino.ino
+++ farmduino-arduino/farmduino-arduino.ino
@@ -1,9 +1,27 @@
-void setup()
-{
-  
+#include <DHT.h>
+
+#define DHT_PIN 2
+#define DHT_TYPE DHT22
+
+DHT dht(DHT_PIN, DHT_TYPE);
+
+void readDHTData() {
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.print(" °C, Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
 }
 
-void loop()
-{
-  
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  readDHTData();
+  delay(5000);
 }
