be73db6ad673462b373929ac38422b7001d3d2fe
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-09 09:42:02 +0300

    :fire: Frontend: Remove several elements from the navbar component

--- farmduino-desktop/src/components/user-navbar/navbar.js
+++ farmduino-desktop/src/components/user-navbar/navbar.js
@@ -7,24 +7,16 @@ const profile = process.env.PUBLIC_URL + '/assets/icons/profile.png';
 const Navbar = () => {
 
   return (
-    <div className={styles.body}>
       <div className={styles.main_container}>
-        <div className={styles.text_container}>
           <h3 className={styles.name}>
             Hussein Turfah
           </h3>
-        </div>
         <Link href='/notifications' className={styles.logo_container}>
           <Badge badgeContent={1} color="error">
             <img src={notifications} alt="logo" className={styles.logo} />
           </Badge>
         </Link>
-        <Link href='/profile' className={styles.logo_container}>
-          <img src={profile} alt="logo" className={styles.logo} />
-        </Link>
-        <div className={styles.logo_container}><User  /></div>
-      </div>
-    </div>
-  )
+      </div>  
+      )
 }
 export default Navbar;
