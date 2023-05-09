e3e4e2e8de774061ea2aca6e3ab23e089a5e868c
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-09 09:51:36 +0300

    :fire: Frontend: Add a new google line chart in temperature page

--- farmduino-desktop/src/pages/temperature/temperature.js
+++ farmduino-desktop/src/pages/temperature/temperature.js
@@ -5,21 +5,51 @@ import Ticker from '../../components/ticker/ticker';
 import { Page_Title } from '../../components/general-components/general';
 import {Plant_row} from '../../components/dashboard-components/plant';
 import styles from './temperature.module.css';
+import { Chart } from 'react-google-charts';
+
 
 const Temperature = () => {
+  function generateData() {
+    const data = [['X', 'Temperature']];
+    for (let i = 0; i < 10; i++) {
+      const temperature = Math.floor(Math.random() * 20) + 20;
+      data.push([i, temperature]);
+    }
+    return data;
+  }
+  const data = generateData();
   return (
     <div className="body">
       <Sidebar />
       <div className="main_container">
         <Navbar />
-        <Ticker />
         <div className="submain_container">
           <Page_Title title="Temperature" subtitle="Greenhouse 1" />
-          <div className="graph container"></div>
+          <div className="graph container">
+            <Chart
+              width={'100%'}
+              height={'90vh'}
+              chartType="LineChart"
+              loader={<div>Loading Chart</div>}
+              data={data}
+              options={{
+                hAxis: {
+                  title: 'Day Time',
+                },
+                vAxis: {
+                  title: 'Temperature( °C)',
+                },
+                curveType:"function",
+                title: 'Temperature over Time',
+              }}
+              rootProps={{ 'data-testid': '1' }}
+            />
+          </div>
           <div className={styles.plant}>
             <Plant_row />
           </div>
         </div>
+<Ticker />
       </div>
     </div>
   );
