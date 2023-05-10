12c48b1c1fb1107b35c51f55354f85e743171cdb
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-10 09:22:29 +0300

    :fire: Frontend: Add several components to admin dashboard page

--- farmduino-desktop/src/pages/admin panel/dashboard/admin-dashboard.js
+++ farmduino-desktop/src/pages/admin panel/dashboard/admin-dashboard.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import styles from './admin-dashboard.module.css'
+import AdminSidebar from '../../../components/user-sidebar/admin-sidebar'
+import { Admin_Page_Title, Container_title, Page_Title } from '../../../components/general-components/general';
+import Ticker from '../../../components/ticker/ticker';
+import AdminNavbar from '../../../components/user-navbar/admin-navbar';
+import UsersTable from '../../../components/admin-components/table/users-table';
+
 
 const AdminDashboard = () => {
+
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
+    <div className='body'>
+      <AdminSidebar />
+      <div className='main_container'>
+        <AdminNavbar />
+        <div className='submain_container'>
+          <Admin_Page_Title title="Dashboard" subtitle="Greenhouse 1" />
+          <div className={styles.container1}>
+            <Container_title title='Users Table' />
+            <UsersTable />
+          </div>
+        </div>
+        <Ticker />
     </div>
-  )
+  </div>
+  );
 }
 export default AdminDashboard
