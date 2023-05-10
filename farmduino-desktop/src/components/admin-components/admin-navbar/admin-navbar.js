e8bc32c1c495a824881519cdd2f9674aab0fb7df
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-10 06:17:02 +0300

    :fire: Frontend: Create a new admin navbar component

---
+++ farmduino-desktop/src/components/admin-components/admin-navbar/admin-navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './admin-navbar.module.css'
+import { Link } from 'react-router-dom'
+
+
+
+const Admin_navbar = () => {
+  return (
+    <div className={styles.container}>
+     
+    </div>
+  );
+}
