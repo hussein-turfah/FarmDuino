47ae969a91bc6005d4c37cfcd4e318001f350082
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-10 08:50:33 +0300

    :link: Frontend: Link the frontend with the backend to list the users later:star:

--- farmduino-desktop/src/components/admin-components/table/users-table.js
+++ farmduino-desktop/src/components/admin-components/table/users-table.js
@@ -1,15 +1,12 @@
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from '@mui/material';
-import * as React from 'react';
-
+import React, { useEffect, useState } from 'react'; 
+import UseHttp from '../../../hooks/http-request';
 
 
 const UsersTable = () => {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const rows=[
-    {id: 1, name: 'John Doe', email: '', role: 'Admin', status: 'Active'},
-  ]
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [users, setUsers] = React.useState([]);
 
   const handleChangePage = (event: unknown, newPage: number) => {
     setPage(newPage);
@@ -19,6 +16,24 @@ const UsersTable = () => {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
+  useEffect(() => {
+
+    const userData = async () => {
+      try {
+        const users = await UseHttp("admin/users", "GET", "",{Authorization: "bearer " + localStorage.getItem("token")})
+        const rowdata = []
+
+        for (let i = 0; i < users.length; i++) {
+          rowdata.push(users[i]['id'],users[i]['first_name'],users[i]['last_name'], users[i]['email'], users[i]['plant_name'], users[i]['is_admin'])
+        }
+        setUsers(users);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    userData();
+  }, []);
+
 
   return (
     <div>
@@ -27,18 +42,16 @@ const UsersTable = () => {
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
-              
-            </TableRow>
+             </TableRow>
           </TableHead>
           <TableBody>
-            
           </TableBody>
         </Table>
       </TableContainer>
       <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
+        rowsPerPageOptions={[10, 25, 50]}
         component="div"
-        count={rows.length}
+        count={users.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
