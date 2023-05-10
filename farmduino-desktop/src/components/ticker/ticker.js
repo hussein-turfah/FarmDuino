9d9ace4b617f8dbce582cbbd7c8d88007bfdd860
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-10 11:57:00 +0300

    :link: Frontend: Create a function for getting ticker elements from the database

--- farmduino-desktop/src/components/ticker/ticker.js
+++ farmduino-desktop/src/components/ticker/ticker.js
@@ -1,11 +1,38 @@
+import React, { useEffect, useState } from 'react';
 import styles from './ticker.module.css'; 
+import UseHttp from '../../hooks/http-request';
 
 const Ticker = () => {
+  const [Ticker, setTicker] = useState([])
+
+
+  useEffect(() => {
+    const data = [];
+    const tickerData = async () => {
+      try {
+        const ticker = await UseHttp("ticker-elements", "GET", "",{Authorization: "bearer " + localStorage.getItem("token")})
+        for(let i=0;i < ticker.length;i++){
+          data.push(ticker[i]['product_name'] + " : " + ticker[i]['price'] + " $")  
+        }
+        setTicker(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    tickerData();
+  }, []);
+
   return(
-    <div className={styles.body}>
-      <p className={styles.text}>
-        This is the ticker of the market prices of the vegetables and fruits.
-      </p>
+    <div className={styles.ticker}>
+      <ul>
+        {
+          Ticker.map((ticker) => (
+            <li className={styles.ticker__item}>
+              {ticker}
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
