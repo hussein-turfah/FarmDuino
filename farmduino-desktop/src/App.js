f7a087e35790419a02b60d83d401e4a4ed46ad6e
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-11 21:53:44 +0300

    :fire: Frontend: Add more routing conditions for the ease of navigation in admin and user pages

--- farmduino-desktop/src/App.js
+++ farmduino-desktop/src/App.js
@@ -18,20 +18,25 @@ function App() {
   return (
       <Routes>
         <Route path="/register" element={<Register/>} />
-        <Route path="/login" element={<Login/>} />
-        {localStorage.getItem("token") ? 
+        <Route path="*" element={<Login/>} />
+        if (localStorage.getItem("is_admin")==1) {
+          <>
+          Navigate ("/admin-dashboard")
+          <Route path="/admin-dashboard" element={<AdminDashboard/>} />
+          <Route path="/admin-sensors" element={<Admin_sensors />} />
+          </>
+        }else if(localStorage.getItem("token")  ){
         <>
+          Navigate ("/dashboard")
           <Route path="/dashboard" element={<Dashboard/>} />
           <Route path="/forgot-password" element={<ForgotPassword/>} />
           <Route path="/temperature" element={<Temperature/>} />
           <Route path="/humidity" element={<Humidity/>} />
           <Route path="/soil-moisture" element={<Soil_moisture/>} />
           <Route path="/light-intensity" element={<Light_intensity/>} />
-          <Route path="/admin-dashboard" element={<AdminDashboard/>} />
-          <Route path="/admin-sensors" element={<Admin_sensors />} />
         </>
-        :
-        <Route path="/login" />
+        }else{
+          Navigate ("*")
         }
       </Routes>
   );
