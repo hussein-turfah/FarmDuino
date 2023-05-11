8129196b3947d4eb846801cb2952c138286dc662
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-11 22:18:23 +0300

    :space_invader::bug: Frontend: Fix a bug in the google line graph in the light intensity page which prevented the graph from being displayed

--- farmduino-desktop/src/pages/light-intensity/light-intensity.js
+++ farmduino-desktop/src/pages/light-intensity/light-intensity.js
@@ -5,8 +5,34 @@ import Ticker from '../../components/ticker/ticker';
 import { Page_Title } from '../../components/general-components/general';
 import {Plant_row} from '../../components/dashboard-components/plant';
 import styles from './light-intensity.module.css';
+import UseHttp from '../../hooks/http-request';
+import Chart from 'react-google-charts';
 
 const Light_intensity = () => {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    const data = []
+    const getData = async () => {
+      try {
+        const response = await UseHttp("user-data", "GET", "", {Authorization: "bearer "+ localStorage.getItem("token")})
+        const chartData = response.map((item) => {
+          if(item.name === "light_intensity") {
+            const date = new Date(item.created_at).getHours() + ":" + new Date(item.created_at).getMinutes()
+            const value = Number(item.value);
+            data.push([date, value]);
+          }else {
+            return null;
+          }
+        }).filter((item) => item !== null); 
+        
+        setData(data);
+      } catch(error) {
+        console.log(error);
+      }
+    };
+    
+    getData();
+  }, []);
   return (
     <div className="body">
       <Sidebar />
@@ -14,7 +40,24 @@ const Light_intensity = () => {
         <Navbar />
         <div className="submain_container">
           <Page_Title title="Light Intensity" subtitle="Greenhouse 1" />
-          <div className="graph container"></div>
+          <div className="graph container">
+          <Chart
+            chartType="LineChart"
+            options={{
+              hAxis: {
+                title: 'Time(24h)',
+              },
+              vAxis: {
+                title: 'Light Intensity(Lux)',
+              },
+              title: 'Light Intensity over Time',
+            }}
+            data={[["Date", "Light Intensity"], ...data]}
+            width="100%"
+            height="60vh"
+            legendToggle
+          />
+          </div>
           <div className={styles.plant}>
             <Plant_row />
           </div>
