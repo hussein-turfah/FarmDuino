8e64237117da2dea802ff9615d5ffe6e4c5b57e6
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-17 14:11:09 +0300

    :link: Frontend: Fix a bug in linking AI generated data between backend and frontend

--- farmduino-desktop/src/components/dashboard-components/plant.js
+++ farmduino-desktop/src/components/dashboard-components/plant.js
@@ -1,26 +1,42 @@
 import { useEffect, useState } from "react";
 import { Container_title, Myboxnologo, Page_Content_Container } from "../general-components/general";
 import styles from "./plant.module.css";
+import UseHttp from "../../hooks/http-request";
 
 const plant_image = process.env.PUBLIC_URL + 'assets/images/banana.jpg';
 
 
 export const Plant = (props) => {
   const [aiData, setAIData] = useState([]);
+  const [ideal_conditions, setIdealConditions] = useState([]);
+  const [plant_image, setPlantImage] = useState("")
 
 
-
+  // get artificial intelligence details
   useEffect(() => {
     const artificialIntelligenceDetails = async ()=>{
       try{ 
-        // const data = await UseHttp("ai","GET","",{Authorization: "bearer"+ localStorage.getItem("token")})
-        // setAIData(data);
-        // console.log(data);
+        const data = await UseHttp("ai","GET","",{Authorization: "bearer"+ localStorage.getItem("token")})
+        setAIData(data);
+        setIdealConditions(data.ideal_conditions);
       }catch(error){
         console.log(error)
       }
     }
     artificialIntelligenceDetails();
+  }, []);
+
+  // get plant image
+  useEffect(() => {
+    const plantImage = async ()=>{
+      try{
+        const image = await UseHttp("plant-image","GET","",{Authorization: "bearer"+ localStorage.getItem("token")})
+        setPlantImage(image.image_url)
+      }catch(error){
+        console.log(error)
+      }
+    }
+    plantImage();
   }, []);
 
   return (
@@ -29,18 +45,18 @@ export const Plant = (props) => {
       <div className={styles.plant_details_container}>
       <div className={styles.first_row}>
         <div className={styles.plant_image_container}>
-          <img src={plant_image} className={styles.plant_image} />
+          <img src={plant_image} className={styles.plant_image} alt="Loading..." />
         </div>
        <Myboxnologo styles={styles.plant_species} 
-       title="Plant Species" value="BANANA (MUSA)"/>
+       title="Plant Species" value={aiData.Genus_species}/>
       </div>
       <div className={styles.second_row}>
-        <Myboxnologo styles={styles.ai_readings} title="Temperature" value="25°C" />
-        <Myboxnologo styles={styles.ai_readings} title="Humidity" value="20%" />
+        <Myboxnologo styles={styles.ai_readings} title="Temperature" value={ideal_conditions.temperature} />
+        <Myboxnologo styles={styles.ai_readings} title="Humidity" value={ideal_conditions.humidity} />
       </div>
       <div className={styles.second_row}>
-        <Myboxnologo styles={styles.ai_readings} title="Soil Moisture" value="50%" />
-        <Myboxnologo styles={styles.ai_readings} title="Light Intensity" value="50 lx" />
+        <Myboxnologo styles={styles.ai_readings} title="Soil Moisture" value={ideal_conditions.soil_moisture} />
+        <Myboxnologo styles={styles.ai_readings} title="Light Intensity" value={ideal_conditions.light_intensity} />
       </div>
     </div>
     </div>
