039d62b66d404992e8725a4ffe4022695b05f0b7
Hussein Turfah <husseinturfah299@gmail.com>
2023-05-17 17:54:26 +0300

    :star_struck: Frontend: Rearranged several elements in the plant component

--- farmduino-desktop/src/components/dashboard-components/plant.js
+++ farmduino-desktop/src/components/dashboard-components/plant.js
@@ -3,6 +3,8 @@ import { Container_title, Myboxnologo, Page_Content_Container } from "../general
 import styles from "./plant.module.css";
 import UseHttp from "../../hooks/http-request";
 
+const logo = process.env.PUBLIC_URL + "assets/icons/artificial-intelligence.png";
+
 
 export const Plant = (props) => {
   const [aiData, setAIData] = useState([]);
@@ -39,24 +41,29 @@ export const Plant = (props) => {
 
   return (
     <div className={styles.plant_body}>
-      <Container_title title="Plant Details" />
-      <div className={styles.plant_details_container}>
-      <div className={styles.first_row}>
-        <div className={styles.plant_image_container}>
-          <img src={plant_image} className={styles.plant_image} alt="Plant Image" />
+      <div className={styles.container_title}>
+        <Container_title title="Ideal Conditions" />
+        <div className={styles.container_logo}>
+          <img src={logo} className={styles.logo} alt="Logo" />
         </div>
-       <Myboxnologo styles={styles.plant_species} 
-       title="Plant Species" value={aiData.Genus_species}/>
       </div>
-      <div className={styles.second_row}>
-        <Myboxnologo styles={styles.ai_readings} title="Temperature" value={ideal_conditions.temperature} />
-        <Myboxnologo styles={styles.ai_readings} title="Humidity" value={ideal_conditions.humidity} />
+      <div className={styles.plant_details_container}>
+        <div className={styles.first_row}>
+          <div className={styles.plant_image_container}>
+            <img src={plant_image} className={styles.plant_image} alt="Plant Image" />
+          </div>
+          <Myboxnologo styles={styles.plant_species} 
+          title="Plant Species" value={aiData.Genus_species}/>
+        </div>
+        <div className={styles.second_row}>
+          <Myboxnologo styles={styles.ai_readings} title="Temperature" value={ideal_conditions.temperature} />
+          <Myboxnologo styles={styles.ai_readings} title="Humidity" value={ideal_conditions.humidity} />
+        </div>
+        <div className={styles.second_row}>
+          <Myboxnologo styles={styles.ai_readings} title="Soil Moisture" value={ideal_conditions.soil_moisture} />
+          <Myboxnologo styles={styles.ai_readings} title="Light Intensity" value={ideal_conditions.light_intensity} />
+        </div>
       </div>
-      <div className={styles.second_row}>
-        <Myboxnologo styles={styles.ai_readings} title="Soil Moisture" value={ideal_conditions.soil_moisture} />
-        <Myboxnologo styles={styles.ai_readings} title="Light Intensity" value={ideal_conditions.light_intensity} />
-      </div>
-    </div>
     </div>
   );
 };
@@ -78,7 +85,12 @@ export const Plant_row = (props) => {
 
   return (
     <div className={styles.row_plant_body}>
-      <Container_title title="Consult AI" />
+      <div className={styles.container_title}>
+        <Container_title title="Ideal Conditions" />
+        <div className={styles.container_logo}>
+          <img src={logo} className={styles.logo} alt="Logo" />
+        </div>
+      </div>
       <div className={styles.row_plant_details_container}>
       <div className={styles.row_first_row}>
         <div className={styles.row_plant_image_container}>
